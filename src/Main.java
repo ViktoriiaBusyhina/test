4ef76da204456b5ecad7ded4e0fa0a23ee42e39b
viktoria <viiikiii.g@gmail.com>
2023-02-15 19:11:35 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        int x;
+
+        for (int i : arr) {
+
+            x = i;
+            System.out.print(x + " ");
+        }
+    }
+    }
